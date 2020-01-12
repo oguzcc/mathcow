e5908720a8b8c545de803ca96ed7e639f4fedf5e
oguzcc <oguzcc@gmail.com>
2020-01-12 22:32:31 +0300

    12 ocak 1030 son temiz

--- routes/settings.js
+++ routes/settings.js
@@ -6,7 +6,7 @@ const winston = require("winston");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", [auth, admin], async (req, res) => {
+router.get("/", [auth], async (req, res) => {
   const queryResult = await req.query;
   const settings = await Setting.find(queryResult).select("-_id");
   res.send(settings);
