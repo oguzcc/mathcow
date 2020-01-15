efccbd9840faecac190b076207b2236e8aa3a778
oguzcc <oguzcc@gmail.com>
2020-01-15 23:05:09 +0300

    points sort ve validation questions

--- routes/users.js
+++ routes/users.js
@@ -28,16 +28,7 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
 
-  user = new User(
-    _.pick(req.body, [
-      "name",
-      "email",
-      "password",
-      "isAdmin",
-      "points",
-      "accuracyPercentage"
-    ])
-  );
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
