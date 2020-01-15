498920cf15fd251353d29f17fb39b2c00c540bc0
oguzcc <oguzcc@gmail.com>
2020-01-15 23:03:36 +0300

    points sort ve validation questions

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 module.exports = function() {
   mongoose
-    .connect("mongodb://localhost:27017/playground", {
+    .connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
