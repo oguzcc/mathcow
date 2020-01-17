07a1e787cb11d38921d8fa75d2efd923b6c3ad5d
oguzcc <oguzcc@gmail.com>
2020-01-17 17:54:55 +0300

    put duzenlendi

--- routes/questions.js
+++ routes/questions.js
@@ -102,7 +102,7 @@ router.post("/", [auth, admin], async (req, res) => {
   res.send(question);
 });
 
-/* router.put("/:id", [auth, admin, validateObjectId], async (req, res) => {
+router.put("/:id", [auth, admin, validateObjectId], async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -124,41 +124,7 @@ router.post("/", [auth, admin], async (req, res) => {
       .send("The question with the given ID was not found.");
 
   res.send(question);
-}); */
-
-router.put(
-  "/:topicID/:cardID/:questionID",
-  [auth, admin, validateObjectId],
-  async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const topicID = req.params.topicID;
-    const cardID = req.params.cardID;
-    const questionID = req.params.questionID;
-
-    const question = await Question.findByIdAndUpdate(
-      { topicID: topicID, cardID: cardID, questionID: questionID },
-      {
-        $set: {
-          topicID: req.body.topicID,
-          cardID: req.body.cardID,
-          questionID: req.body.questionID,
-          question: req.body.question,
-          answers: req.body.answers
-        }
-      },
-      { new: true }
-    );
-
-    if (!question)
-      return res
-        .status(404)
-        .send("The question with the given ID was not found.");
-
-    res.send(question);
-  }
-);
+});
 
 router.delete("/:id", [auth, admin, validateObjectId], async (req, res) => {
   const question = await Question.findByIdAndRemove(req.params.id);
