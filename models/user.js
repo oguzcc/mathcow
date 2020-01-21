9522c9c8bf02561bf4823edb728be0d8c3c7dc05
oguzcc <oguzcc@gmail.com>
2020-01-21 16:33:14 +0300

    user duzenlendi

--- models/user.js
+++ models/user.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    minlength: 8,
+    minlength: 5,
     maxlength: 50
   },
   email: {
@@ -76,7 +76,7 @@ const User = mongoose.model("User", userSchema);
 function validateUser(user) {
   const schema = {
     name: Joi.string()
-      .min(8)
+      .min(5)
       .max(50)
       .required(),
     email: Joi.string()
