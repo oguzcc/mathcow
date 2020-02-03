2cd0839746d8a60aee7ebf8e6c617d9222e05b89
oguzcc <oguzcc@gmail.com>
2020-02-03 20:33:47 +0300

    oto restful

--- routes/restful.js
+++ routes/restful.js
@@ -6,33 +6,68 @@ const express = require("express");
 const router = express.Router();
 
 router.put(
-  "/:topicID/:cardID/:beginN/:endN",
+  "/:typeQ/:topicID/:cardID/:begini/:endi/:beginj/:endj",
   [auth, admin],
   async (req, res) => {
+    const typeQ = req.params.typeQ;
     const topicID = req.params.topicID;
     const cardID = req.params.cardID;
-    const beginN = req.params.beginN;
-    const endN = req.params.endN;
+    const begini = req.params.begini;
+    const endi = req.params.endi;
+    const beginj = req.params.beginj;
+    const endj = req.params.endj;
 
     let questionID = 101;
     const trainingQuestion = [];
     const layoutType = [0, 1, 2];
 
+    let q00;
+    let a00;
+    let a01;
+    let a02;
+    let a03;
+    let a04;
+    let a05;
+    let answers;
+
     const svgBegin =
       "<svg viewBox='0 0 64 64'><text x='32' y='40' text-anchor='middle' font-size='32' fill='#FFFFFF' font-family='RobotoMono'>";
     const svgEnd = "</text></svg>";
-    for (let i = beginN; i < endN; i++) {
-      for (let j = beginN; j < endN; j++) {
-        let q00 = `${svgBegin}${i} + ${j}${svgEnd}`;
-        let ii = parseInt(i);
-        let jj = parseInt(j);
-        let a00 = `${svgBegin}${ii + jj}${svgEnd}`;
-        let a01 = `${svgBegin}${ii + jj - 2}${svgEnd}`;
-        let a02 = `${svgBegin}${ii + jj - 1}${svgEnd}`;
-        let a03 = `${svgBegin}${ii + jj + 1}${svgEnd}`;
-        let a04 = `${svgBegin}${ii + jj + 2}${svgEnd}`;
-        let a05 = `${svgBegin}${ii + jj + 3}${svgEnd}`;
-        let answers = [
+    for (let i = begini; i < endi; i++) {
+      for (let j = beginj; j < endj; j++) {
+        if (typeQ == 1) {
+          q00 = `${svgBegin}${i} + ${j}${svgEnd}`;
+          let ii = parseInt(i);
+          let jj = parseInt(j);
+          a00 = `${svgBegin}${ii + jj}${svgEnd}`;
+          a01 = `${svgBegin}${ii + jj - 2}${svgEnd}`;
+          a02 = `${svgBegin}${ii + jj - 1}${svgEnd}`;
+          a03 = `${svgBegin}${ii + jj + 1}${svgEnd}`;
+          a04 = `${svgBegin}${ii + jj + 2}${svgEnd}`;
+          a05 = `${svgBegin}${ii + jj + 3}${svgEnd}`;
+        } else if (typeQ == 2) {
+          q00 = `${svgBegin}${j} - ${i}${svgEnd}`;
+          ii = parseInt(i);
+          jj = parseInt(j);
+          a00 = `${svgBegin}${ii - jj}${svgEnd}`;
+          a01 = `${svgBegin}${ii - jj - 1}${svgEnd}`;
+          a02 = `${svgBegin}${ii - jj + 1}${svgEnd}`;
+          a03 = `${svgBegin}${ii - jj + 2}${svgEnd}`;
+          a04 = `${svgBegin}${ii - jj + 3}${svgEnd}`;
+          a05 = `${svgBegin}${ii - jj + 4}${svgEnd}`;
+        } else if (typeQ == 3) {
+          q00 = `${svgBegin}${j} x ${i}${svgEnd}`;
+          ii = parseInt(i);
+          jj = parseInt(j);
+          a00 = `${svgBegin}${ii * jj}${svgEnd}`;
+          a01 = `${svgBegin}${ii * jj - 1}${svgEnd}`;
+          a02 = `${svgBegin}${ii * jj + 1}${svgEnd}`;
+          a03 = `${svgBegin}${ii * jj + 2}${svgEnd}`;
+          a04 = `${svgBegin}${ii * jj + 3}${svgEnd}`;
+          a05 = `${svgBegin}${ii * jj + 4}${svgEnd}`;
+        }
+
+        answers = [
           { answer: a00, isCorrect: true },
           { answer: a01, isCorrect: false },
           { answer: a02, isCorrect: false },
